9826c78a66c10b54c22cbd6b35894365efcadc16
Karl Consing <karl.consing@gmail.com>
2018-07-20 22:47:06 -0500

    worked on fallfoolishness drafter UI

--- FallFoolishnessDrafter/MainWindow.xaml.cs
+++ FallFoolishnessDrafter/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.IO;
 namespace FallFoolishnessDrafter
 {
     /// <summary>
@@ -20,10 +20,13 @@ namespace FallFoolishnessDrafter
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<string> players;
+
         public MainWindow()
         {
             InitializeComponent();
             this.DataContext = this;
+            LoadDraftBoard();
         }
 
         public string BackgroundPath
@@ -33,5 +36,52 @@ namespace FallFoolishnessDrafter
                 return Environment.CurrentDirectory + @"\bg1.jpg";
             }
         }
+
+        public string PicturePath
+        {
+            get
+            {
+                return Environment.CurrentDirectory + @"\pic1.jpg";
+            }
+        }
+
+        public void LoadDraftBoard()
+        {
+            try
+            {
+                string[] lines;
+                string[] tokens;
+                players = new List<string>();
+
+                lines = File.ReadAllLines(Environment.CurrentDirectory + @"\players.txt");
+
+                foreach (string line in lines)
+                {
+                    tokens = line.Split('|');
+                    player player = new player(tokens[0], tokens[1]);
+
+                    DraftPool.Items.Add(line);
+                    //DraftPool.Items.Add(player.lastName + ", " + player.firstName);
+                }
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
     }
+
+    public class player
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public player(string firstname = "fist", string lastname = "last")
+        {
+            firstName = firstname;
+            lastName = lastname;
+        }
+        
+    }
+
 }
